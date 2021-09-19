a613e051b19e2fdb88fafa4598a096a231fa5094
spenna <sebapenna19@gmail.com>
2021-09-19 19:59:09 -0300

    Cargar incompatibilidades y costos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import constantes
 
+
 def escribir_solucion(cantidad_prendas):
     output = open("solucion.txt", "w")
     for i in range(1, cantidad_prendas):
@@ -8,9 +9,26 @@ def escribir_solucion(cantidad_prendas):
     val = str(cantidad_prendas)
     output.write(f'{val} {val}')
 
+
+def cargar_incompatibilidad(incompatibilidades, prenda1, prenda2):
+    incomp_prenda1 = incompatibilidades.get(prenda1)
+    if incomp_prenda1:
+        incomp_prenda1.append(prenda2)
+    else:
+        incomp_prenda1 = [prenda2]
+
+    incompatibilidades[prenda1] = incomp_prenda1
+
+
+def cargar_costo(costos, prenda, costo):
+    costos[prenda] = costo
+
+
 def main():
     cant_incompatibilidades = 0
     cant_prendas = 0
+    incompatibilidades = {}
+    costos = {}
 
     with open("primer_problema.txt") as archivo:
         for linea in archivo:
@@ -19,16 +37,24 @@ def main():
             if tipo_dato == constantes.COMENTARIO:
                 continue
 
-            linea = linea.rstrip('\n')    # Quitar caracter nueva linea
+            linea = linea.rstrip('\n')  # Quitar caracter nueva linea
             datos = linea.split(constantes.DATOS_SEP)
 
             if tipo_dato == constantes.FORMATO_PROBLEMA:
                 cant_incompatibilidades = int(datos[constantes.POS_CANT_INCOMPATIBILIDADES])
                 cant_prendas = int(datos[constantes.POS_CANT_PRENDAS])
             elif tipo_dato == constantes.INCOMPATIBILIDAD:
-                pass # todo: cargar incompatibilidades
+                cargar_incompatibilidad(
+                    incompatibilidades,
+                    int(datos[constantes.POS_PRENDA_INCOMPATIBLE_1]),
+                    int(datos[constantes.POS_PRENDA_INCOMPATIBLE_2])
+                )
             elif tipo_dato == constantes.TIEMPO_LAVADO:
-                pass # todo: cargar costos
+                cargar_costo(
+                    costos,
+                    int(datos[constantes.POS_PRENDA_LAVADO]),
+                    int(datos[constantes.POS_TIEMPO_LAVADO])
+                )
             else:
                 raise Exception("Contenido de linea incorrecto: " + tipo_dato)
 
